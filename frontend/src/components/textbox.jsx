ca66adc5333304ef0b07b052ff758b6f336cf1aa
NicoM-7 <90717917+Nico-M7@users.noreply.github.com>
2023-01-29 21:02:44 -0500

    Create working textbox component. Will be used in the future to create and edit outlines.

---
+++ frontend/src/components/textbox.jsx
@@ -0,0 +1,40 @@
+import React, { useState } from "react";
+import "../styles/buttons.css";
+
+const Textbox = ({ defaultValue }) => {
+    const [value, setValue] = useState(defaultValue);
+    const [editing, setEditing] = useState(false);
+    const [originalValue, setOriginalValue] = useState(defaultValue);
+
+    const handleEdit = () => {
+        setEditing(true);
+    };
+
+    const handleSave = () => {
+        setEditing(false);
+        setOriginalValue(value);
+    };
+
+    const handleCancel = () => {
+        setEditing(false);
+        setValue(originalValue);
+    };
+
+    return (
+        <div>
+            {!editing && <p>{value}</p>}
+            {!editing && (
+                <button className="edit" onClick={handleEdit}>Edit</button>
+            )}
+            {editing && (
+                <div>
+                    <input value={value} onChange={(event) => setValue(event.target.value)} />
+                    <button className="save" onClick={handleSave}>Save</button>
+                    <button className="cancel" onClick={handleCancel}>Cancel</button>
+                </div>
+            )}
+        </div>
+    );
+};
+
+export default Textbox;
