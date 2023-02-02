218ff340b3e364c052b87b2b4f93ebe3500023b8
golivott <golivott@uwo.ca>
2023-02-02 18:51:24 -0500

    added funcitonality for change api

--- backend/routes/changeRouter.js
+++ backend/routes/changeRouter.js
@@ -3,6 +3,7 @@ const db = require('../DBConnect.js');
 
 const changeRouter = express.Router();
 
+// Add a change to an outline in db
 changeRouter.post("", (req, res) => {
     db.query(`INSERT INTO change (dateTime, section, content, comment, outlineID, authorID) VALUES (?, ?, ?, ?, ?, ?)`,
         [
@@ -12,6 +13,22 @@ changeRouter.post("", (req, res) => {
             req.body.comment,
             req.body.outlineID,
             req.body.authorID
+        ],
+        (err, data) => {
+            if (err) {
+                res.json(err);
+            }
+            else {
+                res.json(data);
+            }
+        })
+});
+
+// Get all changes for an outline
+changeRouter.get("", (req, res) => {
+    db.query(`SELECT * FROM change WHERE outlineID=?`,
+        [
+            req.query.outlineID
         ],
         (err, data) => {
             if (err) {
