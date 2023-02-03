8973324ab74e319a952bfd4a08f698c3dcd61055
golivott <golivott@uwo.ca>
2023-02-02 20:33:10 -0500

    Added error handling for user creation with no role

--- backend/routes/userRouter.js
+++ backend/routes/userRouter.js
@@ -5,21 +5,26 @@ const userRouter = express.Router();
 
 // Adds that given user to db
 userRouter.post("", (req, res) => {
-    db.query("INSERT INTO user VALUES (?, ?, ?, ?);",
-        [
-            req.body.userID,
-            req.body.admin,
-            req.body.instructor,
-            req.body.reviewer
-        ],
-        (err, data) => {
-            if (err) {
-                res.json(err);
-            }
-            else {
-                res.json(data);
-            }
-        })
+    if (req.body.admin || req.body.instructor || req.body.reviewer) {
+        db.query("INSERT INTO user VALUES (?, ?, ?, ?);",
+            [
+                req.body.userID,
+                req.body.admin,
+                req.body.instructor,
+                req.body.reviewer
+            ],
+            (err, data) => {
+                if (err) {
+                    res.status(400).json(err);
+                }
+                else {
+                    res.json(data);
+                }
+            })
+    }
+    else {
+        res.status(400).json("User must have a role");
+    }
 });
 
 // Gets user information with the given username
