5e60606278a1a4c4e8e1c881e40b7225d4eff630
golivott <golivott@uwo.ca>
2023-02-03 00:39:38 -0500

    Created api calls related to courses

---
+++ backend/routes/courseRouter.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const db = require('../DBConnect.js');
+
+const courseRouter = express.Router();
+
+courseRouter.post("", (req, res) => {
+    db.query(`INSERT INTO course (courseID, courseName, courseReviewer) VALUES (?, ?, ?);`,
+        [
+            req.body.courseID,
+            req.body.courseName,
+            req.body.courseReviewer
+        ],
+        (err, data) => {
+            if (err) {
+                res.status(400).json(err);
+            }
+            else {
+                res.json(data);
+            }
+        })
+});
+
+courseRouter.get("/:courseID", (req, res) => {
+    db.query(`SELECT * FROM course WHERE courseID=?`, [req.params.courseID], (err, data) => {
+        if (err) {
+            res.status(400).json(err);
+        }
+        else {
+            res.json(data);
+        }
+    })
+});
+
+module.exports = courseRouter;
--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ const userRouter = require("./routes/userRouter");
 const outlineRouter = require("./routes/outlineRouter");
 const changeRouter = require("./routes/changeRouter");
 const userCourseRouter = require("./routes/userCourseRouter");
+const courseRouter = require('./routes/courseRouter');
 
 const app = express();
 
@@ -22,6 +23,7 @@ app.use("/api/user", userRouter);
 app.use("/api/outline", outlineRouter);
 app.use("/api/change", changeRouter);
 app.use("/api/userCourse", userCourseRouter);
+app.use("/api/course", courseRouter);
 
 app.listen(9000, () => {
     console.log("Listening on port 9000");
