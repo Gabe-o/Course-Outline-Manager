81c4f8ddd68c82a5f80177af064208bd56c3f37c
golivott <golivott@uwo.ca>
2023-02-03 12:29:42 -0500

    change router is now modification router to avoid sql keyword

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ const cors = require("cors");
 
 const userRouter = require("./routes/userRouter");
 const outlineRouter = require("./routes/outlineRouter");
-const changeRouter = require("./routes/changeRouter");
+const modificationRouter = require("./routes/modificationRouter");
 const userCourseRouter = require("./routes/userCourseRouter");
 const courseRouter = require('./routes/courseRouter');
 
@@ -21,7 +21,7 @@ app.use((req, res, next) => {
 // Routes
 app.use("/api/user", userRouter);
 app.use("/api/outline", outlineRouter);
-app.use("/api/change", changeRouter);
+app.use("/api/modification", modificationRouter);
 app.use("/api/userCourse", userCourseRouter);
 app.use("/api/course", courseRouter);
 
