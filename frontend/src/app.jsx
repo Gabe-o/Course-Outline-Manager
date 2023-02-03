98ffc06e2d27bb15d8cfdccbb9415d578886466d
golivott <golivott@uwo.ca>
2023-02-02 19:26:38 -0500

    Removed Test api calls

--- frontend/src/app.jsx
+++ frontend/src/app.jsx
@@ -1,28 +1,13 @@
 import axios from "axios";
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-import { useState } from "react";
 import CreateOutline from "./pages/createOutline";
 
 function App() {
-
-  const [test, setTest] = useState("orignal");
-  axios.get('/test')
-    .then(function (response) {
-      console.log("Entered");
-      setTest(response.data)
-    })
-    .catch(function (error) {
-      console.log(error);
-    });
-
   return (
     <BrowserRouter>
       <div>
-        <h1>{test}</h1>
-      </div>
-      <div>
-      <CreateOutline></CreateOutline>
+        <CreateOutline></CreateOutline>
       </div>
       <Routes>
 
