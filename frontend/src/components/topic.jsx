97f034dfbe63ae906e3a324c607234605aab3da5
NicoM-7 <90717917+Nico-M7@users.noreply.github.com>
2023-02-03 20:07:50 -0500

    Created a working evaluation component for the create outline page

---
+++ frontend/src/components/topic.jsx
@@ -0,0 +1,143 @@
+import { useState } from "react";
+
+const Topic = ({ topic, topicsList, topicsListSetter }) => {
+
+    const [topicValue, setTopicValue] = useState(topic);
+    const [descriptionValue, setDescriptionValue] = useState("");
+    const [indicatorValue, setIndicatorValue] = useState("");
+    const [points, setPoints] = useState([]);
+    const [addingPoints, setAddingPoints] = useState(false);
+    const [saving, setSaving] = useState(false);
+    const [cancelling, setCancelling] = useState(false);
+    const [editing, setEditing] = useState(false);
+    const [editedPoints, setEditedPoints] = useState([]);
+
+    const initialValues = points.map(point => ({
+        description: point.description,
+        indicator: point.indicator,
+    }));
+
+    const handleDescriptionChange = (i, event) => {
+        setEditedPoints({
+            ...editedPoints,
+            [i]: {
+                ...editedPoints[i],
+                description: event.target.value,
+            }
+        });
+    };
+
+    const handleIndicatorChange = (i, event) => {
+        setEditedPoints({
+            ...editedPoints,
+            [i]: {
+                ...editedPoints[i],
+                indicator: event.target.value,
+            }
+        });
+    };
+
+    const addPoints = () => {
+        setAddingPoints(true);
+        setSaving(false);
+        setCancelling(false);
+        setEditing(false);
+        setDescriptionValue("");
+        setIndicatorValue("");
+    }
+
+    const cancelPoints = () => {
+        setCancelling(true);
+        setSaving(false);
+        setAddingPoints(false);
+        setEditing(false);
+        setDescriptionValue("");
+        setIndicatorValue("");
+    }
+
+    const savePoints = () => {
+        setSaving(true);
+        setCancelling(false);
+        setAddingPoints(false);
+        setEditing(false);
+        const point = {
+            description: descriptionValue,
+            indicator: indicatorValue,
+        }
+        setPoints([...points, point]);
+    }
+
+    const deleteTopic = () => {
+        topicsListSetter(topicsList.filter(element => element !== topic));
+    }
+
+    const editTopic = () => {
+        setEditing(true);
+        setSaving(false);
+        setCancelling(false);
+        setAddingPoints(false);
+    }
+
+    const saveEditChanges = () => {
+        if (topicValue !== topic) {
+            if (topicsList.indexOf(topic) !== -1) {
+                topicsList[topicsList.indexOf(topic)] = topicValue;
+                topicsListSetter(topicsList);
+            }
+        }
+        if (editedPoints.length != 0) {
+            for (let i = 0; i < initialValues.length; i++) {
+                if (initialValues[i].description !== (editedPoints[i].description ? editedPoints[i].description : initialValues[i].description)) {
+                    points[i].description = editedPoints[i].description;
+                }
+
+                if (initialValues[i].indicator !== (editedPoints[i].indicator !== undefined ? editedPoints[i].indicator : initialValues[i].indicator)) {
+                    points[i].indicator = editedPoints[i].indicator;
+                }
+            }
+        }
+        setEditing(false);
+        setPoints(points);
+        setEditedPoints([]);
+    }
+
+    const cancelEditChanges = () => {
+
+    }
+
+    const intToLetter = (num) => {
+        return String.fromCharCode(97 + num);
+    }
+
+    return (
+        <div>
+            <h5>{topicValue}</h5>
+            <button onClick={addPoints}>Add Points</button>
+            <button onClick={deleteTopic}>Delete</button>
+            <button onClick={editTopic}>Edit</button>
+            {points.length > 0 ? points.map((point, i) => <p><b>{intToLetter(i) + ") "}</b>{point.description + " " + point.indicator}</p>) : ""}
+            {addingPoints ?
+                <div>
+                    <input value={descriptionValue} placeholder="Enter Description" onChange={(event) => setDescriptionValue(event.target.value)} />
+                    <input value={indicatorValue} placeholder="Enter GA Indicator" onChange={(event) => setIndicatorValue(event.target.value)} />
+                    <button onClick={savePoints}>Save</button>
+                    <button onClick={cancelPoints}>Cancel</button>
+                </div> : ""}
+            {editing ?
+                <div>
+                    <input defaultValue={topic} placeholder="Edit Topic Name" onChange={(event) => setTopicValue(event.target.value)} />
+                    <br />
+                    {points.length > 0 ? points.map((point, i) =>
+                        <div>
+                            <label><b>{intToLetter(i) + ") "}</b></label>
+                            <input defaultValue={point.description} placeholder="Edit Description" onChange={(event) => handleDescriptionChange(i, event)} />
+                            <input defaultValue={point.indicator} placeholder="Edit GA Indicator" onChange={(event) => handleIndicatorChange(i, event)} />
+                        </div>) : ""}
+                    <button onClick={saveEditChanges}>Save</button>
+                    <button onClick={cancelEditChanges}>Cancel</button>
+                </div> : ""}
+        </div>
+    );
+}
+
+export default Topic;
--- frontend/src/pages/createOutline.jsx
+++ frontend/src/pages/createOutline.jsx
@@ -3,105 +3,133 @@ import Textbox from "../components/textbox";
 import Counter from "../components/counter";
 import Switch from "../components/creditsSwitch";
 import LearningObjectivesDropdown from "../components/learningObjectivesDropdown";
+import TopicCreator from "../components/topicCreator";
+import Evaluation from "../components/evaluation";
 
 const CreateOutline = () => {
 
+    const handleSubmit = () => {
+
+    }
     return (
         <div>
             <h1>Description</h1>
-            <Textbox></Textbox>
+            <Textbox />
             <h1>Instructor</h1>
             <h3>Name</h3>
-            <Textbox></Textbox>
+            <Textbox />
             <h3>Phone Number</h3>
-            <Textbox></Textbox>
+            <Textbox />
             <h3>Extension</h3>
-            <Textbox></Textbox>
+            <Textbox />
             <h3>UWO Email</h3>
-            <Textbox></Textbox>
+            <Textbox />
             <h3>Consulation Hours</h3>
-            <Textbox></Textbox>
+            <Textbox />
             <h1>Acedemic Calender Copy</h1>
             <h1>Contact Hours</h1>
             <h3>Lecture Hours</h3>
-            <Counter></Counter>
+            <Counter />
             <h3>Labratory Hours</h3>
-            <Counter></Counter>
+            <Counter />
             <h3>Tutorial Hours</h3>
-            <Counter></Counter>
+            <Counter />
             <h3>Credits</h3>
-            <Switch></Switch>
+            <Switch />
             <h1>Antirequisite</h1>
-            <Textbox></Textbox>
+            <Textbox />
             <h1>Prerequisites</h1>
-            <Textbox></Textbox>
+            <Textbox />
             <h1>Co-requisite</h1>
-            <Textbox></Textbox>
+            <Textbox />
             <h1>CEAB Academic Units</h1>
             <h3>Engineering Science %</h3>
-            <Textbox></Textbox>
+            <Textbox />
             <h3>Engineering Design %</h3>
-            <Textbox></Textbox>
+            <Textbox />
             <h1>Required Textbook</h1>
-            <Textbox></Textbox>
+            <Textbox />
             <h1>Other Required References</h1>
-            <Textbox></Textbox>
+            <Textbox />
             <h1>Recommended References</h1>
-            <Textbox></Textbox>
-            <h1>General Learning Objectives (CEAB Graduate Attributes)</h1>
-            <p>I is introductory, D is intermediate, A is advanced and X is empty</p>
-            <h3>Knowledge Base</h3>
-            <LearningObjectivesDropdown></LearningObjectivesDropdown>
+            <Textbox />
+            <h1>General Learning Objectives (CEAB)</h1>
             <h3>Use of Engineering Tools</h3>
-            <LearningObjectivesDropdown></LearningObjectivesDropdown>
+            <LearningObjectivesDropdown />
+            <h5>Comments</h5>
+            <Textbox />
             <h3>Impact on Society and the Environment</h3>
-            <LearningObjectivesDropdown></LearningObjectivesDropdown>
+            <LearningObjectivesDropdown />
+            <h5>Comments</h5>
+            <Textbox />
             <h3>Problem Analysis</h3>
-            <LearningObjectivesDropdown></LearningObjectivesDropdown>
+            <LearningObjectivesDropdown />
+            <h5>Comments</h5>
+            <Textbox />
             <h3>Individual and Team Work</h3>
-            <LearningObjectivesDropdown></LearningObjectivesDropdown>
+            <LearningObjectivesDropdown />
+            <h5>Comments</h5>
+            <Textbox />
             <h3>Ethics and Equity</h3>
-            <LearningObjectivesDropdown></LearningObjectivesDropdown>
+            <LearningObjectivesDropdown />
+            <h5>Comments</h5>
+            <Textbox />
             <h3>Investigation</h3>
-            <LearningObjectivesDropdown></LearningObjectivesDropdown>
+            <LearningObjectivesDropdown />
+            <h5>Comments</h5>
+            <Textbox />
             <h3>Communication Skills</h3>
-            <LearningObjectivesDropdown></LearningObjectivesDropdown>
+            <LearningObjectivesDropdown />
+            <h5>Comments</h5>
+            <Textbox />
             <h3>Economics and Project Management</h3>
-            <LearningObjectivesDropdown></LearningObjectivesDropdown>
+            <LearningObjectivesDropdown />
+            <h5>Comments</h5>
+            <Textbox />
             <h3>Design</h3>
-            <LearningObjectivesDropdown></LearningObjectivesDropdown>
+            <LearningObjectivesDropdown />
+            <h5>Comments</h5>
+            <Textbox />
             <h3>Professionalism</h3>
-            <LearningObjectivesDropdown></LearningObjectivesDropdown>
+            <LearningObjectivesDropdown />
+            <h5>Comments</h5>
+            <Textbox />
             <h3>Life-Long Learning</h3>
-            <LearningObjectivesDropdown></LearningObjectivesDropdown>
+            <LearningObjectivesDropdown />
+            <h5>Comments</h5>
+            <Textbox />
             <h1>Topics</h1>
+            <TopicCreator />
             <h1>Evaluation</h1>
+            <Evaluation />
             <h3>Course Components, Weights, Descriptions</h3>
             <h3>Evaluation Description</h3>
-            <Textbox></Textbox>
+            <Textbox />
             <h1>Late Submission Policy</h1>
-            <Textbox></Textbox>
+            <Textbox />
             <h1>Assignment Submission Locker</h1>
-            <Textbox></Textbox>
+            <Textbox />
             <h1>Use of English</h1>
-            <Textbox></Textbox>
+            <Textbox />
             <h1>Attendance</h1>
-            <Textbox></Textbox>
+            <Textbox />
             <h1>Absence Due to Illnes or Other Circumstances</h1>
-            <Textbox></Textbox>
+            <Textbox />
             <h1>Missed Midterm Examinations</h1>
-            <Textbox></Textbox>
+            <Textbox />
             <h1>Cheating and Plagiarism</h1>
-            <Textbox></Textbox>
+            <Textbox />
             <h1>Use of Electronic Devices</h1>
-            <Textbox></Textbox>
+            <Textbox />
             <h1>Use of Personal Response Devices ("Clickers")</h1>
-            <Textbox></Textbox>
+            <Textbox />
             <h1>Policy on Repating All Components of a Course</h1>
             <Textbox></Textbox>
             <h1>Internet and Electronic Mail</h1>
-            <Textbox></Textbox>
+            <Textbox />
             <h1>Accessibility</h1>
+            <Textbox />
+            <button onClick={handleSubmit}>Submit</button>
         </div>
     )
 }
