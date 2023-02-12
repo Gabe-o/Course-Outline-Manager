ae3fe0c2a46ef3ca5469d1515ef075d4c3c454f3
NicoM-7 <90717917+Nico-M7@users.noreply.github.com>
2023-02-12 03:52:57 -0500

    Changed firebase api key

--- backend/firebase.js
+++ backend/firebase.js
@@ -1,21 +1,17 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-
-
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyBdL05Y5FHCJhSha3LvZ-urtSttLhQNzCM",
-    authDomain: "se3350-7172b.firebaseapp.com",
-    projectId: "se3350-7172b",
-    storageBucket: "se3350-7172b.appspot.com",
-    messagingSenderId: "391588091227",
-    appId: "1:391588091227:web:b5a8c45344ac9221eff67e",
-    measurementId: "G-2VRKYG5ETT"
+    apiKey: "AIzaSyDtQdT_-8_R1KUo9T3DIoVNFLxVCa_qk_I",
+    authDomain: "se3350-508c4.firebaseapp.com",
+    projectId: "se3350-508c4",
+    storageBucket: "se3350-508c4.appspot.com",
+    messagingSenderId: "383673428382",
+    appId: "1:383673428382:web:db2981609a6093b4b70e8f"
 };
 
 // Initialize Firebase
